340a7a26150a78d0bb06e8a8156ff0fb2b4780c8
n-laszlo-m <55386821+n-laszlo-m@users.noreply.github.com>
2020-04-19 05:25:01 +0200

    setting up the frontend-ng groundwork, many hacks

--- WebApplication1/wwwroot/hexcanvas.js
+++ WebApplication1/wwwroot/hexcanvas.js
@@ -33,7 +33,7 @@ function setup() {
 		for (let j = 0; j < 200; j++)
 			cellGrid.push(new HexCell(j, i));
 
-    console.log(cellGrid);
+	console.log(cellGrid);
 }
 
 function prev() {
@@ -51,7 +51,7 @@ function next() {
 async function requestRound() {
 	// https://localhost:44339/api/GetSimulation?id=1284113452&round=1&x=0&y=0
 	let response = await fetch("api/GetSimulation?id=" + simID["simulationID"] + "&round=" + document.getElementById("simID").value + "&x=0&y=0");
-    console.log(response);
+	console.log(response);
 	let myJson = await response.json();
 	console.log(myJson);
 
@@ -61,18 +61,18 @@ async function requestRound() {
 
 async function initSimulation(roundcount) {
 	let simID = 0;
-    if (!simLaunched) simID = await fetch("api/StartSimulation/" + roundcount);
-    simLaunched = true;
-    return simID;
+	if (!simLaunched) simID = await fetch("api/StartSimulation/" + roundcount);
+	simLaunched = true;
+	return simID;
 }
 
 async function loadServerData() {
-    simID = await initSimulation(200);
-    simID = await simID.json();
-    console.log(simID["simulationID"]);
+	simID = await initSimulation(200);
+	simID = await simID.json();
+	console.log(simID["simulationID"]);
 
-    let response = await fetch("api/GetSimulation?id=" + simID["simulationID"] + "&round=1&x=0&y=0");
-    console.log(response);
+	let response = await fetch("api/GetSimulation?id=" + simID["simulationID"] + "&round=1&x=0&y=0");
+	console.log(response);
 	let myJson = await response.json();
 	console.log(myJson);
 
@@ -88,14 +88,14 @@ function draw() {
 	stroke(144, 144, 144);
 	strokeWeight(1);
 	cellGrid.filter(elem => elem.gridRefX >= vpMinCorner[0] &&
-		                    elem.gridRefX <= vpMaxCorner[0] &&
-		                    elem.gridRefY >= vpMinCorner[1] &&
-		                    elem.gridRefY <= vpMaxCorner[1])
-	        .forEach(cell => cell.draw(distX, distY));
+							elem.gridRefX <= vpMaxCorner[0] &&
+							elem.gridRefY >= vpMinCorner[1] &&
+							elem.gridRefY <= vpMaxCorner[1])
+			.forEach(cell => cell.draw(distX, distY));
 
 	loc = findCellAt(mouseX - distX, mouseY - distY);
 	cellGrid.filter(elem => elem.gridRefX == loc[0] && elem.gridRefY == loc[1])
-	        .forEach(cell => cell.draw(distX, distY, [96, 96, 128]));
+			.forEach(cell => cell.draw(distX, distY, [96, 96, 128]));
 
 	stroke(0);
 	strokeWeight(2);
@@ -139,7 +139,14 @@ class HexCell {
 		this.gridRefX = x;
 		this.gridRefY = y;
 
+		this.uid = -1;
+
 		this.content = "";
+		this.wiring = [];
+		this.startState = -1;
+		this.parentID = -1;
+
+		this.health = -1;
 
 		this.vertices = this.precalc(x, y);
 	}
@@ -180,9 +187,9 @@ async function updateChunk(grid, src) {
 	grid.forEach(elem => {
 		src.filter(input => input.X == elem.gridRefX && input.Y == elem.gridRefY)
 		   .forEach(item => {
-		    	if (item.Type == "t") elem.content = "🌳";
-		    	else if (item.Type == "l") elem.content = "🦁";
-		    	else if (item.Type == "a") elem.content = "🧠";
+				if (item.Type == "t") elem.content = "🌳";
+				else if (item.Type == "l") elem.content = "🦁";
+				else if (item.Type == "a") elem.content = "🧠";
 		   })
 	});
 }
@@ -213,4 +220,72 @@ async function updateChunk2(grid, src) {
 	document.getElementById("simID").setAttribute('onchange', 'requestRound()');
 	document.getElementById("prevButton").setAttribute('onchange', 'prev()');
 	document.getElementById("nextButton").setAttribute('onchange', 'next()');
+}
+
+async function updateChunk3(grid, src) {
+	// make buttons inactive, log incoming json data
+	document.getElementById("simID").setAttribute('onchange', '');
+	document.getElementById("prevButton").setAttribute('onchange', '');
+	document.getElementById("nextButton").setAttribute('onchange', '');
+	console.log(src);
+
+	if (document.getElementById("simID").value == 0) {
+		// initial state esetén a data után kajtatunk
+		let objReference = null;
+		src.data.forEach(cell => {
+			objReference = grid.find(elem => 
+				elem.gridRefX == cell.X && elem.gridRefY == cell.Y);
+
+			if (cell.Type == "t") objReference.content = "🌳";
+			else if (cell.Type == "l") objReference.content = "🦁";
+			else if (cell.Type == "a") {
+				objReference.content = "🧠";
+				objReference.wiring = cell.wiring;
+				objReference.startState = cell.startState;
+				objReference.health = 50;
+			}
+
+			objReference.uid = cell.UID;
+		});
+	} else {
+		// subsequent statek esetén a movements, spawns, deaths,
+		// healthupdates után kajtatunk
+
+		let objReference = null;
+
+		src.movements.forEach(cell => {
+			objReference = grid.find(elem => elem.uid == cell.uid);
+			objReference.gridRefX = cell.X;
+			objReference.gridRefY = cell.Y;
+		};
+
+		src.spawns.forEach(cell => {
+			objReference = grid.find(elem => 
+				elem.gridRefX == cell.X && elem.gridRefY == cell.Y);
+			
+			objReference.content = "🧠";
+			objReference.uid = cell.childUID;
+			objReference.parentID = cell.parentUID;
+			objReference.startState = cell.startState;
+			objReference.wiring = cell.wiring;
+			objReference.health = 50;
+		};
+
+		src.deaths.forEach(cell => {
+			objReference = grid.find(elem => 
+				elem.gridRefX == cell.X && elem.gridRefY == cell.Y);
+			
+			objReference.content = "";
+			objReference.uid = -1;
+			objReference.parentID = -1;
+			objReference.startState = -1;
+			objReference.wiring = [];
+			objReference.health = -1;
+		};
+
+		src.healthUpdates.forEach(cell => {
+			objReference = grid.find(elem => elem.uid == cell.uid);
+			objReference.health = cell.health;
+		};
+	}
 }
