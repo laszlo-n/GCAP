cc58521d67096565a38e37794ac083ae4207fa75
Ádám Szilárd <adamszilard7@protonmail.com>
2020-04-20 09:53:53 +0200

    more statistics + command system

--- Stat/FamilyTreeBuilder.cs
+++ Stat/FamilyTreeBuilder.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 
@@ -8,7 +10,16 @@ namespace Stat
 {
     class FamilyTreeBuilder
     {
-        public static List<FamilyTreeItem> BuildTree(int simID)
+        private static int currentSim = -1;
+        private static ReadOnlyCollection<FamilyTreeItem> families;
+
+        public static void Unload()
+        {
+            currentSim = -1;
+            families = null;
+        }
+
+        public static List<FamilyTreeItem> LoadTree(int simID)
         {
             List<FamilyTreeItem> results = new List<FamilyTreeItem>();
             Dictionary<int, FamilyTreeItem> tmpDic = new Dictionary<int, FamilyTreeItem>();
@@ -56,8 +67,80 @@ namespace Stat
             }
 
             results = results.OrderByDescending(e => e.RecursiveCount).ToList();
+            
+            FamilyTreeBuilder.currentSim = simID;
+            FamilyTreeBuilder.families = new ReadOnlyCollection<FamilyTreeItem>(results);
 
             return results;
+        }
+
+        public static bool IsLoaded { get { return FamilyTreeBuilder.currentSim != -1; } }
+
+        public static List<(int from, string through, int to)> GetWiring(int automatonID)
+        {
+            if(!FamilyTreeBuilder.IsLoaded)
+            {
+                throw new InvalidOperationException("No simulation is loaded.");
+            }
+
+            List<(int, string, int)> result = new List<(int, string, int)>();
+
+            using(StreamReader be = new StreamReader($"{Program.GetSimDir(currentSim)}/initial.gcasim"))
+            {
+                while(!be.EndOfStream)
+                {
+                    JSONArray data = new JSONObject(be.ReadLine()).GetArrayChild("data");
+                    for(int i = 0; i < data.Count; i ++)
+                    {
+                        JSONObject content = (JSONObject)data[i];
+                        if(content.GetStringChild("Type") == "a" && content.GetIntChild("UID") == automatonID)
+                        {
+                            JSONArray wiring = content.GetArrayChild("wiring");
+                            for(int j = 0; j < wiring.Count; j ++)
+                            {
+                                JSONObject stateChange = (JSONObject)wiring[j];
+                                result.Add((stateChange.GetIntChild("from"), stateChange.GetStringChild("through"), stateChange.GetIntChild("to")));
+                            }
+
+                            return result;
+                        }
+                    }
+                }
+            }
+
+            string dir = Program.GetSimDir(currentSim);
+            for(int i = 1; File.Exists($"{dir}/round{i}.gcasim"); i ++)
+            {
+                using(StreamReader be = new StreamReader($"{dir}/round{i}.gcasim"))
+                {
+                    while(!be.EndOfStream)
+                    {
+                        JSONArray spawns = new JSONObject(be.ReadLine()).GetArrayChild("spawns");
+                        for(int j = 0; j < spawns.Count; j ++)
+                        {
+                            JSONObject content = (JSONObject)spawns[j];
+                            if(content.GetIntChild("childUID") == automatonID)
+                            {
+                                JSONArray wiring = content.GetArrayChild("wiring");
+                                for(int k = 0; k < wiring.Count; k ++)
+                                {
+                                    JSONObject stateChange = (JSONObject)wiring[k];
+                                    result.Add((stateChange.GetIntChild("from"), stateChange.GetStringChild("through"), stateChange.GetIntChild("to")));
+                                }
+
+                                return result;
+                            }
+                        }
+                    }
+                }
+            }
+
+            throw new KeyNotFoundException("No automaton exists with the given ID.");
+        }
+
+        public static ReadOnlyCollection<FamilyTreeItem> Families
+        {
+            get { return families; }
         }
     }
 }
--- Stat/Program.cs
+++ Stat/Program.cs
@@ -1,15 +1,125 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Stat
 {
     class Program
     {
+        static Dictionary<int, string> directions = new Dictionary<int, string>
+        {
+            { 0, "B " },
+            { 1, "BL" },
+            { 2, "JF" },
+            { 3, "J " },
+            { 4, "JL" },
+            { 5, "BL" }
+        };
+
         static void Main(string[] args)
         {
-            Console.Write("Szimuláció ID: ");
-            int simID = int.Parse(Console.ReadLine());
+            while(true)
+            {
+                Console.Write("> ");
+                string[] command = Console.ReadLine().Split(' ');
+                switch(command[0])
+                {
+                    case "load":
+                        try
+                        {
+                            FamilyTreeBuilder.LoadTree(int.Parse(command[1]));
+                            Console.WriteLine("Szimuláció betöltve");
+                        }
+                        catch(Exception)
+                        {
+                            Console.WriteLine("Szimuláció betöltése sikertelen");
+                        }
+                        break;
+                    case "unload":
+                        FamilyTreeBuilder.Unload();
+                        break;
+                    case "exit":
+                        return;
+                    case "automaton":
+                        int id;
+                        if(int.TryParse(command[1], out id))
+                        {
+                            List<(int, string, int)> wiring = FamilyTreeBuilder.GetWiring(id);
+                            foreach((int from, string through, int to) in wiring)
+                            {
+                                Console.WriteLine($"{directions[from]}--{through}-->{directions[to]}");
+                            }
+                        }
+                        else
+                        {
+                            switch(command[1])
+                            {
+                                case "list":
+                                    ReadOnlyCollection<FamilyTreeItem> families = FamilyTreeBuilder.Families;
+                                    
+                                    Action<FamilyTreeItem, int> listWithChildren = null;
+                                    listWithChildren = (item, layer) =>
+                                    {
 
-            FamilyTreeBuilder.BuildTree(simID);
+                                        for(int i = 0; i < layer - 1; i ++)
+                                        {
+                                            Console.Write("| ");
+                                        }
+                                        if(layer != 0)
+                                        {
+                                            Console.Write("|-");
+                                        }
+
+                                        Console.WriteLine(item.UID);
+                                        foreach(var child in item.Children)
+                                        {
+                                            listWithChildren(child, layer + 1);
+                                        }
+                                    };
+
+                                    for(int i = 0; i < families.Count; i ++)
+                                    {
+                                        listWithChildren(families[i], 0);
+                                    }
+                                    break;
+                                case "by-family-size":
+                                    Dictionary<int, int> familyCounts = new Dictionary<int, int>();
+                                    for(int i = 0; i < FamilyTreeBuilder.Families.Count; i ++)
+                                    {
+                                        int count = FamilyTreeBuilder.Families[i].RecursiveCount;
+                                        if(familyCounts.ContainsKey(count))
+                                        {
+                                            familyCounts[count] ++;
+                                        }
+                                        else
+                                        {
+                                            familyCounts.Add(count, 1);
+                                        }
+                                    }
+
+                                    List<(int, int)> tmpList = new List<(int, int)>();
+                                    foreach(KeyValuePair<int, int> element in familyCounts)
+                                    {
+                                        tmpList.Add((element.Key, element.Value));
+                                    }
+
+                                    tmpList = tmpList.OrderByDescending(e => e.Item1).ToList();
+
+                                    foreach((int size, int count) in tmpList)
+                                    {
+                                        Console.WriteLine($"{size} tagú család: {count} db");
+                                    }
+                                    break;
+                            }
+                        }
+
+                        break;
+                    default:
+                        Console.WriteLine("Imseretlen parancs");
+                        break;
+                }
+            }
         }
 
         public static string GetSimDir(int uid)
